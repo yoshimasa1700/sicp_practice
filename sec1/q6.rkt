2f8eb3b8606e0f0e7724eb2ea69702a4b79750c7
Masahiko Toyoshi <m.yoshi.1700@gmail.com>
2016-01-07 18:11:55 +0900

    q6

---
+++ sec1/q6.rkt
@@ -0,0 +1,27 @@
+#lang racket
+
+
+(define (average x y) (/ (+ x y) 2))
+
+(define (improve guess x) (average (/ x guess) guess))
+
+(define (good-enough? guess x)
+	(<
+		(abs (- (* guess guess) x))
+		0.0001)
+)
+
+(define (new-if predicate then-clause else-clause)
+	(cond (predicate then-clause)
+	(else else-clause))
+)
+
+(define (sqrt-iter guess x)
+	(if (good-enough? guess x)
+		guess
+		(sqrt-iter (improve guess x) x))
+)
+
+(define (sqrt x) (sqrt-iter 1.0 x))
+
+(sqrt 2)
